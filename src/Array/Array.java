d4b5cadbd72a6a5caa1bb021eb2e2ad67065c6bd
dong <dongrui9297@gmail.com>
2020-01-17 12:17:55 +0800

    使用泛型的Array

---
+++ src/Array/Array.java
@@ -0,0 +1,144 @@
+package Array;
+
+public class Array<E> {
+
+    private E[] data;
+    private int size;
+
+    /**
+     * @param capacity 数组容量
+     */
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void add(int index, E e) {
+        if (size == data.length)
+            throw new IllegalArgumentException("AddLast failed. Array is full.");
+
+        if (index < 0 || index > data.length)
+            throw new IllegalArgumentException("AddLast failed. Require index >= 0 and index <= size");
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index > data.length)
+            throw new IllegalArgumentException("Get failed. Require index >= 0 and index <= size");
+        return data[index];
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index > data.length)
+            throw new IllegalArgumentException("Set failed. Require index >= 0 and index <= size");
+        data[index] = e;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(e))
+                return true;
+        }
+        return false;
+    }
+
+    public int find(E e) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(e))
+                return i;
+        }
+        return -1;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index > data.length)
+            throw new IllegalArgumentException("Remove failed. Require index >= 0 and index <= size");
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++)
+            data[i = 1] = data[i];
+        size--;
+        data[size] = null; //Not necessary. It's a loitering object.
+        return ret;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1)
+            remove(index);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        Array<Integer> arr = new Array<Integer>(20);
+        for(int i=0;i<10;i++)
+            arr.addLast(i);
+        System.out.println(arr);
+
+        arr.add(1,100);
+        System.out.println(arr);
+
+        arr.addFirst(-1);
+        System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
+
+    }
+}
